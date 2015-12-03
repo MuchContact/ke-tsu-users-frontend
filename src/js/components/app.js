3774ce70e1c9df05150e5f44cd366052379d2324
hjliu <hjliu@thoughtworks.com>
2015-12-03 10:20:46 +0800

    add title bar

--- src/js/components/app.js
+++ src/js/components/app.js
@@ -10,24 +10,15 @@ const App = React.createClass({
           <nav className="navbar navbar-default">
             <div className="container-fluid">
               <div className="navbar-header">
-                <button type="button" className="navbar-toggle collapsed" data-toggle="collapse"
-                        data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-                  <span className="sr-only">Toggle navigation</span>
-                  <span className="icon-bar"></span>
-                  <span className="icon-bar"></span>
-                  <span className="icon-bar"></span>
-                </button>
-                <a className="navbar-brand" href="#">Brand</a>
+                <a class="navbar-brand" href="#">
+                  <img alt="Brand" src="https://c.na32.content.force.com/servlet/servlet.ImageServer?id=01550000000Yo0H&oid=00D500000007lOm&lastMod=1350290446000"/>
+                </a>
               </div>
 
               <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                <ul className="nav navbar-nav navbar-right">
-                  <li><Link to="/solutions" href="javascript: void(0)">Solutions</Link></li>
-                  <li><Link to="/projects" href="javascript: void(0)">Projects</Link></li>
-                  <li><Link to="/exam_requests" href="javascript: void(0)">Exam Requests</Link></li>
-                  <li><a href="#">Xu Shanchuan</a></li>
+                <ul className="nav navbar-nav navbar-left">
+                  <li><Link to="/" href="javascript: void(0)">Profile</Link></li>
                   <li><a href="#">Logout</a></li>
-                  <li><Link to="/login" href="javascript: void(0)">Login</Link></li>
                 </ul>
               </div>
             </div>
