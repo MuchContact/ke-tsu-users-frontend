b4280a1722f3fb3e9905503307e36c8db2f97c65
xushanchuan <aisensiy@163.com>
2015-12-09 00:56:10 +0800

    show instruction to push code in evaluation

--- src/js/components/evaluations.js
+++ src/js/components/evaluations.js
@@ -17,12 +17,15 @@ const Evaluations = React.createClass({
   componentWillMount() {
     this.props.dispatch(EvaluationAction(`/${this.props.current_user.id}/evaluations`));
   },
+  generateUserName(evaluation) {
+    return `${evaluation.user.name}${evaluation.created_at}`;
+  },
   generateRepositoryUri(evaluation){
-    var repository = "https://";
-    if(evaluation.stack)
+    var repository = "";
+    if (evaluation.stack)
       repository += evaluation.stack.name;
     repository += '-' + evaluation.projectName;
-    if(evaluation.solution)
+    if (evaluation.solution)
       repository += evaluation.solution.name;
 
     var random = Math.floor((Math.random() * 1000) + 1);
@@ -32,56 +35,59 @@ const Evaluations = React.createClass({
   renderStackTags(services){
     var tags = services.map((service) => {
       return (
-        <img alt={service.name} src={service.image_url}/>
+          <img alt={service.name} src={service.image_url}/>
       );
     });
     return (
-      <div className="image-tag-container">
-        {tags}
-      </div>
+        <div className="image-tag-container">
+          {tags}
+        </div>
     );
   },
   render() {
-    var evalus = [];
-    if(this.props.evaluations && this.props.evaluations.length>0){
-      evalus = this.props.evaluations.map((evaluation, index) => {
+    var evaluations = [];
+    if (this.props.evaluations && this.props.evaluations.length > 0) {
+      evaluations = this.props.evaluations.map((evaluation, index) => {
         var status = evaluation.status;
-        if(!evaluation.status)
-          status="NEW";
+        if (!evaluation.status)
+          status = "NEW";
         var repository = this.generateRepositoryUri(evaluation);
         var projectName = this.capitalizeFirstLetter(evaluation.projectName);
 
         var stackTags = this.renderStackTags(evaluation.stack.services);
-       return (
+        return (
             <div className="row evaluation-row">
               <div className="col-md-4">
                 <div className="service-name-trending">
-                {projectName}
+                  {projectName}
                 </div>
                 {stackTags}
               </div>
               <div className="col-md-8">
-                <p>Solution: {evaluation.solution?evaluation.solution.name:''}</p>
-                <p>Stacks: {evaluation.stack?evaluation.stack.name:''}</p>
-                <div className="input-group">
-                  <input type="text" className="form-control"
-                    placeholder="Recipient's username"
-                    aria-describedby="basic-addon2"
-                    value={repository} readonly="readonly" />
-                  <span className="input-group-addon" id="basic-addon2">Copy</span>
+                <div>
+                  <p>Run this commands in your project dir</p>
+                  <pre>deis register http://deis.tw.com --username={this.generateUserName(evaluation)} --password={this.generateUserName(evaluation)} --email={evaluation.user.name}@thoughtworks.com<br/>
+                    deis create {repository} <br/>
+                    ssh-keygen -f ~/.ssh/{repository} -t rsa -N '' <br/>
+                    chmod 400 ~/.ssh/{repository}* <br/>
+                    deis keys:add ~/.ssh/{repository}.pub <br/>
+                    git remote add deis master <br/>
+                    git remote set-url deis ssh://git@deis.deepi.cn:2222/{repository}.git
+                  </pre>
+                  <p>And then you can run <code>git push deis master</code> to submit your code to the repository and waiting for verfication.</p>
                 </div>
               </div>
             </div>
-           );
-     });
+        );
+      });
     }
     return (
-      <div className="row">
-        <div className="page-header">
-          <h3>Evaluations</h3>
+        <div className="row">
+          <div className="page-header">
+            <h3>Evaluations</h3>
+          </div>
+          {evaluations}
         </div>
-        {evalus}
-      </div>
 
     );
   }
